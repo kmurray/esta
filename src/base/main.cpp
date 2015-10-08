432e84210b9365d1e81ecbfcf67d1547bf15a520
kmurray <k.murray@utoronto.ca>
2015-10-08 11:00:11 -0400

    Link to required CUDD libraries

--- src/base/main.cpp
+++ src/base/main.cpp
@@ -6,10 +6,15 @@
 
 #include "blif_parse.hpp"
 
+#include "cudd.h"
+#include "cuddObj.hh"
+
 using std::cout;
 using std::cerr;
 using std::endl;
 using std::string;
+
+void build_bdd(BlifData* blif_data);
 
 int main(int argc, char** argv) {
     if(argc != 2) {
@@ -33,8 +38,32 @@ int main(int argc, char** argv) {
 
     if(blif_data != nullptr) {
         cout << "\tOK" << endl;
+
+        cout << "Building BDD..." << endl;
+        build_bdd(blif_data);
         delete blif_data;
     }
 
     return 0;
+}
+
+
+void build_bdd(BlifData* blif_data) {
+    Cudd mgr();
+    //DdManager* mgr = Cudd_Init(0,0,CUDD_UNIQUE_SLOTS,CUDD_CACHE_SLOTS,0);
+
+    BlifModel* model = blif_data->models[0];
+    
+    BlifNames* names = model->names[0];
+    for(size_t i = 0; i < names->ios.size() - 1; i--) {
+        cout << *names->ios[i] << " ";
+    }
+    cout << "\n";
+    for(auto row_ptr : names->cover_rows) {
+        for(auto logic_val : *row_ptr) {
+            cout << logic_val << " ";
+        }
+        cout << "\n";
+    }
+
 }
