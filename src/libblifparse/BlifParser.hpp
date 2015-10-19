b8ab041d709fb49c4dc7316f8772afd382da0f25
kmurray <k.murray@utoronto.ca>
2015-10-19 13:12:50 -0400

    Squashed 'libs/blifparse/' changes from b29df25..124bf18
    124bf18 Add support for cleaning dangling io ports
    
    git-subtree-dir: libs/blifparse
    git-subtree-split: 124bf18431ed32195253130b08e63df8f7047ccf

--- src/libblifparse/BlifParser.hpp
+++ src/libblifparse/BlifParser.hpp
@@ -13,8 +13,9 @@ class BlifParser {
     public:
         BlifParser()
             : lexer_context_(this)
-            , blif_data_(nullptr) {
-        }
+            , blif_data_(nullptr) 
+            , clean_nets_(true)
+            , sweep_dangling_ios_(true) { }
         virtual ~BlifParser() { }
 
         BlifData* parse(std::string filename);
@@ -24,8 +25,14 @@ class BlifParser {
         BlifData* get_blif_data() { return blif_data_; }
         BlifLexerContext* get_lexer_context() { return &lexer_context_; }
         StringTable* get_str_table() { assert(blif_data_ != nullptr); return &blif_data_->str_table; }
+
+        //Options
+        BlifParser& set_clean_nets(bool val) { clean_nets_ = val; return *this; }
+        BlifParser& set_sweep_dangling_ios(bool val) { sweep_dangling_ios_ = val; return *this; }
     private:
         BlifLexerContext lexer_context_;
         BlifData* blif_data_;
+        bool clean_nets_;
+        bool sweep_dangling_ios_;
 };
 
