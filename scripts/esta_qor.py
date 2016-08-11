a64b2d359a22bc1ac5c102915df1b747a911258a
kmurray <k.murray@utoronto.ca>
2016-08-11 12:56:57 -0400

    Move EMD calculations into re-usable function

--- scripts/esta_qor.py
+++ scripts/esta_qor.py
@@ -56,6 +56,20 @@ def main():
 
         print "EMD {}: ".format(compare_csv), emd(merged_df['probability_x'].values, merged_df['probability_y'].values, distance_matrix)
 
+def hist_emd(reference_hist_df, compare_hist_df):
+    #Merge the two columns on the union of delays
+    merged_df = pd.merge(reference_hist_df, compare_hist_df, how='outer', on=['delay:MAX'])
+
+    #Treat missing values as zero
+    merged_df = merged_df.fillna(0)
+
+    distance_matrix = np.zeros(shape=(merged_df.shape[0], merged_df.shape[0]))
+
+    for i, ival in enumerate(merged_df['delay:MAX'].values):
+        for j, jval in enumerate(merged_df['delay:MAX'].values):
+            distance_matrix[i][j] = abs(ival - jval)
+
+    return emd(merged_df['probability_x'].values, merged_df['probability_y'].values, distance_matrix)
 
 if __name__ == "__main__":
     main()
