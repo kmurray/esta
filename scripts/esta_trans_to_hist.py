c5010a8b91ace47c2955b7fb24db26f37a952a74
kmurray <k.murray@utoronto.ca>
2017-09-21 08:49:40 -0400

    Read inputs as chunks while converting to histograms
    This reduces the peak memory usage

--- scripts/esta_trans_to_hist.py
+++ scripts/esta_trans_to_hist.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import argparse
 from collections import OrderedDict
-from itertools import cycle
 import math
 
 import pandas as pd
@@ -25,17 +24,45 @@ def parse_args():
                         nargs="+",
                         help="delay keys to process in transition_csv")
 
+    parser.add_argument("--input_chunk_size",
+                        default=100,
+                        type=int,
+                        help="How many circuit inputs to load at a time (smaller values decrease peak memory usage in exchange for longer run-time")
+
     return parser.parse_args()
 
 def main():
     args = parse_args()
 
-    print "Loading CSV"
-    trans_delay_df = load_trans_csv(args.transition_csv, keys=args.keys) 
+    hist_dfs = []
 
-    print "Converting to Histogram"
-    hist_df = transitions_to_histograms(trans_delay_df, keys=args.keys)
-    
+
+    ikey = -1 
+    key_groups = []
+    for i, key in enumerate(args.keys):
+
+        if i % args.input_chunk_size == 0:
+            ikey += 1
+            key_groups.append([])
+
+        key_groups[ikey].append(key)
+
+
+    for key_group in key_groups:
+
+        print "Loading CSV for", key_group
+        trans_delay_df = load_trans_csv(args.transition_csv, keys=key_group) 
+
+        print "Converting to Histogram"
+        hist_df = transitions_to_histograms(trans_delay_df, keys=key_group)
+
+        hist_dfs.append(hist_df)
+        
+    print "Merging Dataframes"
+    hist_df = pd.concat(hist_dfs, axis=1)
+    hist_df.sort_index(inplace=True)
+    hist_df.fillna(0., inplace=True)
+
     print "Writing CSV"
     hist_df.to_csv(args.histogram_csv)
 
